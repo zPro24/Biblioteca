92a68de443cf2aed30904f6e7893084160989179
zpro24 <jesusgomezquinterogame@gmail.com>
2026-09-11 22:19:21 -0500

    Refactorizar el estilo de los comentarios en Main.java y añadir el diagrama Biblioteca.drawio

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,27 +2,27 @@ package main;
 
 import javax.swing.JOptionPane;
 
-/**
- * Clase ejecutable que maneja un menú iterativo mediante JOptionPane.
- */
+/*
+    Clase ejecutable que maneja un menú iterativo mediante JOptionPane.
+*/
 public class Main {
 
     public static void main(String[] args) {
-        // Se instancia el gestor con 5 usuarios máximo, 4 estantes y 5 secciones
+        // Se instancia el gestor con 5 usuarios maximo, 4 estantes y 5 secciones
         GestionBiblioteca biblioteca = new GestionBiblioteca(5, 4, 5);
 
         String menu = "=============================================\n"
-                + "    SISTEMA DE GESTIÓN DE BIBLIOTECA (UTS)    \n"
-                + "=============================================\n"
-                + "1. Registrar nuevo usuario (Arreglo 1D)\n"
-                + "2. Mostrar usuarios registrados\n"
-                + "3. Buscar usuario por ID\n"
-                + "4. Mostrar mapa visual de estantes (Matriz 2D)\n"
-                + "5. Actualizar estado de préstamo (Matriz 2D)\n"
-                + "6. Análisis 1: Estadísticas globales de disponibilidad\n"
-                + "7. Análisis 2: Estante con mayor disponibilidad\n"
-                + "0. Salir del programa\n\n"
-                + "Seleccione una opción:";
+                    + "    SISTEMA DE GESTIÓN DE BIBLIOTECA (UTS)    \n"
+                    + "=============================================\n"
+                    + "1. Registrar nuevo usuario (Arreglo 1D)\n"
+                    + "2. Mostrar usuarios registrados\n"
+                    + "3. Buscar usuario por ID\n"
+                    + "4. Mostrar mapa visual de estantes (Matriz 2D)\n"
+                    + "5. Actualizar estado de préstamo (Matriz 2D)\n"
+                    + "6. Análisis 1: Estadísticas globales de disponibilidad\n"
+                    + "7. Análisis 2: Estante con mayor disponibilidad\n"
+                    + "0. Salir del programa\n\n"
+                    + "Seleccione una opción:";
 
         int opcion = -1;
 
@@ -30,7 +30,7 @@ public class Main {
             String input = JOptionPane.showInputDialog(null, menu, "Menú Principal", JOptionPane.QUESTION_MESSAGE);
 
             if (input == null) {
-                // Si presiona Cancelar o cierra la ventana, sale del programa
+                // Si presiona cancelar o cierra la ventana, sale del programa
                 opcion = 0;
             } else {
                 try {
